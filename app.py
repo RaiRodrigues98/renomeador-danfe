6ce2f5d60d2d80a4b89ec23e983c400c6e95bd19
Rai Rodrigues de Sousa <rai.sousa@maglog.tld>
2026-07-29 17:35:18 -0300

    Migração completa para EasyOCR

--- app.py
+++ app.py
@@ -1,16 +1,28 @@
+from pathlib import Path
+
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+
 from routes.download import router as download_router
 from routes.processar import router as processar_router
 
+BASE_DIR = Path(__file__).resolve().parent
+
 app = FastAPI(title="Renomeador DANFE")
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount(
+    "/static",
+    StaticFiles(directory=BASE_DIR / "static"),
+    name="static"
+)
+
+templates = Jinja2Templates(
+    directory=BASE_DIR / "templates"
+)
 
 app.include_router(processar_router)
 app.include_router(download_router)
-templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/")
